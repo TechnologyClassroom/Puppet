124b0cf8c65ed7837a125696d7f507da6a653194
BlueHillBGCB <mmcmahon@bgcb.org>
2016-06-15 15:27:00 -0400

    Create OSXhosts.pp

---
+++ OSXhosts.pp
@@ -0,0 +1,87 @@
+file { '/etc/updatehosts.sh':
+  ensure => present,
+}
+
+file_line { 'Download HOSTSFunix':
+  path => '/etc/updatehosts.sh',
+  ensure => present,
+  line => 'curl -o /etc/HOSTSFunix.txt https://raw.githubusercontent.com/BlueHillBGCB/HOSTS/master/HOSTSFunix.txt',
+}
+
+file_line { 'Download HOSTSMTWRunix':
+  path => '/etc/updatehosts.sh',
+  ensure => present,
+  line => 'curl -o /etc/HOSTSMTWRunix.txt https://raw.githubusercontent.com/BlueHillBGCB/HOSTS/master/HOSTSMTWRunix.txt',
+}
+
+file_line { 'Download HOSTSFunixLS':
+  path => '/etc/updatehosts.sh',
+  ensure => present,
+  line => 'curl -o /etc/HOSTSFunixLS.txt https://raw.githubusercontent.com/BlueHillBGCB/HOSTS/master/HOSTSFunixLS.txt',
+}
+
+file_line { 'Download HOSTSMTWRunixLS':
+  path => '/etc/updatehosts.sh',
+  ensure => present,
+  line => 'curl -o /etc/HOSTSMTWRunixLS.txt https://raw.githubusercontent.com/BlueHillBGCB/HOSTS/master/HOSTSMTWRunixLS.txt',
+}
+
+file { '/etc/HOSTSFunixcp.sh':
+  ensure => present,
+}
+
+file_line { 'cpHOSTSFunix':
+  path => '/etc/HOSTSFunixcp.sh',
+  ensure => present,
+  line => 'cp /etc/HOSTSFunix.txt /etc/hosts',
+  require => File['/etc/HOSTSFunixcp.sh'],
+}
+
+file { '/etc/HOSTSMTWRunixcp.sh':
+  ensure => present,
+}
+
+file_line { 'cpHOSTSMTWRunix':
+  path => '/etc/HOSTSFunixcp.sh',
+  ensure => present,
+  line => 'cp /etc/HOSTSFunix.txt /etc/hosts',
+  require => File['/etc/HOSTSMTWRunixcp.sh'],
+}
+
+file { '/etc/HOSTSFunixLScp.sh':
+  ensure => present,
+}
+
+file_line { 'cpHOSTSFunix':
+  path => '/etc/HOSTSFunixLScp.sh',
+  ensure => present,
+  line => 'cp /etc/HOSTSFunixLS.txt /etc/hosts',
+  require => File['/etc/HOSTSFunixLScp.sh'],
+}
+
+file { '/etc/HOSTSMTWRunixLScp.sh':
+  ensure => present,
+}
+
+file_line { 'cpHOSTSMTWRunixLS':
+  path => '/etc/HOSTSFunixLScp.sh',
+  ensure => present,
+  line => 'cp /etc/HOSTSFunixLS.txt /etc/hosts',
+  require => File['/etc/HOSTSMTWRunixLScp.sh'],
+}
+
+cron { 'CopyHOSTSMTWRunix':
+  ensure      => present,
+  command     => 'sh /etc/HOSTSMTWRunixcp.sh',
+  require => File_line ['cpHOSTSMTWRunix'],
+  user        => 'root',
+  weekday     => '0,1,2,3,4,6',
+}
+
+cron { 'CopyHOSTSFunix':
+  ensure      => present,
+  command     => 'sh /etc/HOSTSFunixcp.sh',
+  require => File_line ['cpHOSTSFunix'],
+  user        => 'root',
+  weekday     => '5',
+}
