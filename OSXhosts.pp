7731debc823861be016cad942a4bfdd2ce992167
BlueHillBGCB <mmcmahon@bgcb.org>
2016-06-16 15:12:08 -0400

    Update OSXhosts.pp

--- OSXhosts.pp
+++ OSXhosts.pp
@@ -6,72 +6,74 @@ file { '/etc/updatehosts.sh':
 }
 
 file_line { 'Download HOSTSFunix':
-  path => '/etc/updatehosts.sh',
-  ensure => present,
+  path    => '/etc/updatehosts.sh',
+  ensure  => present,
   require => File['/etc/updatehosts.sh'],
-  line => 'curl -o /etc/HOSTSFunix.txt https://raw.githubusercontent.com/BlueHillBGCB/HOSTS/master/HOSTSFunix.txt',
+  line    => 'curl -o /etc/HOSTSFunix.txt https://raw.githubusercontent.com/BlueHillBGCB/HOSTS/master/HOSTSFunix.txt',
 }
 
 file_line { 'Download HOSTSMTWRunix':
-  path => '/etc/updatehosts.sh',
-  ensure => present,
+  path    => '/etc/updatehosts.sh',
+  ensure  => present,
   require => File['/etc/updatehosts.sh'],
-  line => 'curl -o /etc/HOSTSMTWRunix.txt https://raw.githubusercontent.com/BlueHillBGCB/HOSTS/master/HOSTSMTWRunix.txt',
+  line    => 'curl -o /etc/HOSTSMTWRunix.txt https://raw.githubusercontent.com/BlueHillBGCB/HOSTS/master/HOSTSMTWRunix.txt',
 }
 
 file_line { 'Download HOSTSFunixLS':
-  path => '/etc/updatehosts.sh',
-  ensure => present,
+  path    => '/etc/updatehosts.sh',
+  ensure  => present,
   require => File['/etc/updatehosts.sh'],
-  line => 'curl -o /etc/HOSTSFunixLS.txt https://raw.githubusercontent.com/BlueHillBGCB/HOSTS/master/HOSTSFunixLS.txt',
+  line    => 'curl -o /etc/HOSTSFunixLS.txt https://raw.githubusercontent.com/BlueHillBGCB/HOSTS/master/HOSTSFunixLS.txt',
 }
 
 file_line { 'Download HOSTSMTWRunixLS':
-  path => '/etc/updatehosts.sh',
-  ensure => present,
+  path    => '/etc/updatehosts.sh',
+  ensure  => present,
   require => File['/etc/updatehosts.sh'],
-  line => 'curl -o /etc/HOSTSMTWRunixLS.txt https://raw.githubusercontent.com/BlueHillBGCB/HOSTS/master/HOSTSMTWRunixLS.txt',
+  line    => 'curl -o /etc/HOSTSMTWRunixLS.txt https://raw.githubusercontent.com/BlueHillBGCB/HOSTS/master/HOSTSMTWRunixLS.txt',
 }
 
+
+
 file_line { 'updatehosts1':
-  path => '/etc/updatehosts.sh',
-  ensure => present,
+  path    => '/etc/updatehosts.sh',
+  ensure  => present,
   require => File_line['Download HOSTSMTWRunixLS'],
-  line => "if [ $(date '+%u') == '5' ];then",
+  line    => "if [ $(date '+%u') == "5' ];then',
 }
 
 file_line { 'updatehosts2':
-  path => '/etc/updatehosts.sh',
-  ensure => present,
+  path    => '/etc/updatehosts.sh',
+  ensure  => present,
   require => File_line['updatehosts1'],
-  line => '    cp /etc/HOSTSFunix.txt /etc/hosts',
+  line    => '    curl -o /etc/HOSTSMTWRunix.txt https://raw.githubusercontent.com/BlueHillBGCB/HOSTS/master/HOSTSMTWRunix.txt',
 }
 
 file_line { 'updatehosts3':
-  path => '/etc/updatehosts.sh',
-  ensure => present,
+  path    => '/etc/updatehosts.sh',
+  ensure  => present,
   require => File_line['updatehosts2'],
-  line => 'else',
+  line    => 'else',
 }
 
 file_line { 'updatehosts4':
-  path => '/etc/updatehosts.sh',
-  ensure => present,
+  path    => '/etc/updatehosts.sh',
+  ensure  => present,
   require => File_line['updatehosts3'],
-  line => '    cp /etc/HOSTSMTWRunix.txt /etc/hosts',
+  line    => '    curl -o /etc/HOSTSMTWRunixLS.txt https://raw.githubusercontent.com/BlueHillBGCB/HOSTS/master/HOSTSMTWRunixLS.txt',
 }
 
 file_line { 'updatehosts5':
-  path => '/etc/updatehosts.sh',
-  ensure => present,
+  path    => '/etc/updatehosts.sh',
+  ensure  => present,
   require => File_line['updatehosts4'],
-  line => 'fi',
+  line    => 'fi',
 }
 
-cron { 'updatehosts':
-  ensure      => present,
-  command     => 'sh /etc/updatehosts.sh',
+cron { 'cronupdatehosts':
+  ensure  => present,
+  command => 'sh /etc/updatehosts.sh',
   require => File['/etc/updatehosts.sh'],
-  user        => 'root',
-  hour        => '*/1',
+  user    => 'root',
+  hour    => '*/1',
 }
