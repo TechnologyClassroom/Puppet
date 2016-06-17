954a3336280eec3a72efba5963941016bdd693bb
BlueHillBGCB <mmcmahon@bgcb.org>
2016-06-17 01:20:27 -0400

    Update and rename installmodules.pp to installmodules.sh

--- installmodules.pp
+++
@@ -1,3 +0,0 @@
-module { 'puppetlabs/stdlib':
-  ensure => present,
-}
---
+++ installmodules.sh
@@ -0,0 +1,7 @@
+# installmodules.sh
+
+# Michael McMahon
+
+# This script installs all of the required puppet modules for these puppet manifests.
+
+sudo puppet module install puppetlabs/stdlib
