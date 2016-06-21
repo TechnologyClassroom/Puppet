ffa862ccfbdadf92d965d91afbd87bb62882868c
BlueHillBGCB <mmcmahon@bgcb.org>
2016-06-21 13:47:22 -0400

    Update mac10.9setup.sh

--- mac10.9setup.sh
+++ mac10.9setup.sh
@@ -2,3 +2,4 @@
 curl -o puppet-agent-1.5.2-1.osx10.9.dmg https://downloads.puppetlabs.com/mac/10.9/PC1/x86_64/puppet-agent-1.5.2-1.osx10.9.dmg
 open puppet-agent-1.5.2-1.osx10.9.dmg
 su Computer\ Clubhouse -c sudo\ puppet\ module\ install\ puppetlabs/stdlib
+su Computer\ Clubhouse -c sudo\ puppet\ agent\ --waitforcert\ 60
