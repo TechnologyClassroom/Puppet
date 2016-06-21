e19e137c8ada8ce65366f6c5bf5700fe70785737
BlueHillBGCB <mmcmahon@bgcb.org>
2016-06-21 13:20:10 -0400

    Create mac10.9setup.sh

---
+++ mac10.9setup.sh
@@ -0,0 +1,4 @@
+# Run this command with sh mac10.9setup.sh
+curl -o puppet-agent-1.5.2-1.osx10.9.dmg https://downloads.puppetlabs.com/mac/10.9/PC1/x86_64/puppet-agent-1.5.2-1.osx10.9.dmg
+open puppet-agent-1.5.2-1.osx10.9.dmg
+su Computer\ Clubhouse -c sudo\ puppet\ module\ install\ puppetlabs/stdlib
